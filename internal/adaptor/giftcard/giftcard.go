06a715fb20fa816f3892bab7865e5f98da61968a
bahramrousta <rousta.bahram@gmail.com>
2024-04-28 09:16:26 +0330

    add customer info func to gift card handler

--- internal/adaptor/giftcard/giftcard.go
+++ internal/adaptor/giftcard/giftcard.go
@@ -1,10 +1,12 @@
 package adaptor
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"giftCard/util"
 	"github.com/rs/zerolog/log"
+	"io"
 	"net/http"
 )
 
@@ -52,4 +54,43 @@ func (g *GiftCard) Auth() (string, error) {
 		return authHeader, nil
 	}
 	return "", errors.New("authentication failed")
+}
+
+func (g *GiftCard) CustomerInfo() (map[string]any, error) {
+	url := "https://sandbox-api.core.hub.gift/customer/info"
+	method := "GET"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return nil, err
+	}
+	token, err := g.Auth()
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Authorization", token)
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+	if res.StatusCode == http.StatusOK {
+		bodyBytes, err := io.ReadAll(res.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		var responseData map[string]any
+
+		err = json.Unmarshal(bodyBytes, &responseData)
+		if err != nil {
+			return nil, err
+		}
+
+		return responseData, nil
+	}
+	return nil, errors.New("failed to get customer info")
 }
