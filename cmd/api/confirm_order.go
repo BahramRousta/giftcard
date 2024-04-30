3ce9d4e75dc7b0a8499142da51dde681a1c9acab
bahramrousta <rousta.bahram@gmail.com>
2024-04-30 12:34:39 +0330

    fix confirm order error

--- cmd/api/confirm_order.go
+++ cmd/api/confirm_order.go
@@ -2,7 +2,7 @@ package api
 
 import (
 	"errors"
-	adaptor "giftCard/internal/adaptor/giftcard"
+	gftErr "giftCard/internal/adaptor/gft_error"
 	"giftCard/internal/service"
 	"github.com/go-playground/validator"
 	"github.com/labstack/echo/v4"
@@ -28,7 +28,7 @@ func NewConfirmOrderHandler(service *service.ConfirmOrderService) *ConfirmOrderH
 func (h *ConfirmOrderHandler) ConfirmOrder(c echo.Context) error {
 	var requestBody confirmOrderRequestBody
 	if err := c.Bind(&requestBody); err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Bad Request"})
+		return c.JSON(http.StatusBadRequest, map[string]string{"gft_error": "Bad Request"})
 	}
 	validate := validator.New()
 	if err := validate.Struct(&requestBody); err != nil {
@@ -36,7 +36,7 @@ func (h *ConfirmOrderHandler) ConfirmOrder(c echo.Context) error {
 		for _, fieldErr := range err.(validator.ValidationErrors) {
 			messages = append(messages, fieldErr.Field()+" is invalid")
 		}
-		return c.JSON(http.StatusBadRequest, map[string]string{"error": strings.Join(messages, ", ")})
+		return c.JSON(http.StatusBadRequest, map[string]string{"gft_error": strings.Join(messages, ", ")})
 	}
 
 	data, err := h.service.OrderConfirmService(requestBody.OrderId)
@@ -48,15 +48,28 @@ func (h *ConfirmOrderHandler) ConfirmOrder(c echo.Context) error {
 				"success": false,
 			})
 		}
-		var orderErr *adaptor.ConfirmOrderError
-		if errors.As(err, &orderErr) {
-			return c.JSON(http.StatusBadRequest, map[string]interface{}{
-				"message": orderErr.ErrorMsg,
-				"data":    orderErr.Response,
+
+		var forbiddenErr *gftErr.ForbiddenErr
+		if errors.As(err, &forbiddenErr) {
+			return c.JSON(http.StatusForbidden, map[string]any{
+				"message": forbiddenErr.ErrMsg,
+				"data":    "",
 				"success": false,
 			})
 		}
-		return c.JSON(http.StatusInternalServerError, map[string]any{"data": "", "message": err.Error(), "success": false})
+		var reqErr *gftErr.RequestErr
+		if errors.As(err, &reqErr) {
+			return c.JSON(http.StatusBadRequest, map[string]any{
+				"message": reqErr.ErrMsg,
+				"data":    reqErr.Response,
+				"success": false,
+			})
+		}
+		return c.JSON(http.StatusInternalServerError, map[string]any{
+			"data":    "",
+			"message": "something went wrong",
+			"success": false,
+		})
 	}
 	return c.JSON(http.StatusCreated, map[string]any{"data": data["data"], "message": "", "success": true})
 }
--- internal/adaptor/giftcard/confirm_order.go
+++ internal/adaptor/giftcard/confirm_order.go
@@ -1,59 +1,28 @@
 package adaptor
 
 import (
-	"bytes"
 	"encoding/json"
-	"io"
-	"net/http"
 )
 
 func (g *GiftCard) ConfirmOrder(orderId string) (map[string]any, error) {
 	url := g.BaseUrl + "/order/confirm"
 	method := "PUT"
-	client := &http.Client{}
 
 	payload := map[string]any{
 		"orderId": orderId,
 	}
+
 	payloadBytes, err := json.Marshal(payload)
 	if err != nil {
 		return nil, err
 	}
 
-	req, err := http.NewRequest(method, url, bytes.NewBuffer(payloadBytes))
+	data, err := g.ProcessRequest(method, url, &payloadBytes)
+
 	if err != nil {
 		return nil, err
 	}
-
-	token, err := g.Auth()
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("Authorization", token)
-
-	res, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-
-	bodyBytes, err := io.ReadAll(res.Body)
-	if err != nil {
-		return nil, err
-	}
-	var responseData map[string]any
-
-	err = json.Unmarshal(bodyBytes, &responseData)
-	if err != nil {
-		return nil, err
-	}
-
-	if res.StatusCode == http.StatusOK {
-		return responseData, nil
-	}
-	return nil, &ConfirmOrderError{ErrorMsg: "failed to create order", Response: responseData}
+	return data, nil
 }
 
 type ConfirmOrderError struct {
