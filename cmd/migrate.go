75c4d4e715e753df58f520ef1e48ea3abb342c4b
bahramrousta <rousta.bahram@gmail.com>
2024-05-04 21:19:12 +0330

    add migrate command

---
+++ cmd/migrate.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"fmt"
+	"giftCard/internal/adaptor/postgres"
+	"github.com/spf13/cobra"
+)
+
+var migrateCmd = &cobra.Command{
+	Use:   "migrate",
+	Short: "Manage database migrations",
+	Long:  `Commands to manage database migrations.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Applying models migration done.")
+		if err := postgres.MigrateModels(); err != nil {
+			fmt.Println("error: ", err)
+			panic(err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(migrateCmd)
+}
---
+++ cmd/root.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"fmt"
+	"giftCard/app"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "giftcard",
+	Short: "Base command for gift card",
+	Long:  "Base command for gift card",
+	Run: func(cmd *cobra.Command, args []string) {
+		app.Start()
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
